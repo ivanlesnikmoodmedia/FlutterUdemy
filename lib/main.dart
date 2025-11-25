f4280b6759d0775e3682b8df292fb5330d08b4a6
Ivan Lesnik <ivan.lesnik@moodmedia.hr>
2025-11-25 15:02:52 +0100

    Half away

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(
     MaterialApp(
@@ -13,31 +14,42 @@ void main() {
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
+  void rollDice() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 63, 5, 120),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(    
-          child: Text(
-            'Hello',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,)
-            ),
-          ),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [ 
+            Image.asset(
+          'assets/images/dice-1.png',
+          width: 250,),
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              padding: const EdgeInsets.only(top: 20,),
+              foregroundColor: Colors.white, 
+              textStyle: TextStyle(
+                fontSize: 28,
+               )
+              ), 
+            child: Text('Roll Dice'))
+        ],
+      ),
       ),
     );
   }
