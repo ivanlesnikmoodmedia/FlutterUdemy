581f5a59a1d6f087fb25456d688b005c4de6e738
Ivan Lesnik <ivan.lesnik@moodmedia.hr>
2025-11-28 13:35:38 +0100

    models

---
+++ lib/expenses.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses ({super.key});
+
+ @override
+ State<StatefulWidget> createState() {
+   throw UnimplementedError();
+   return _ExpensesState();
+   } 
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title:'IvekCourse', 
+      amount: 23.99, 
+      date: DateTime.now(), 
+      category: Category.work,
+    ),
+    Expense(
+      title:'Cinema', 
+      amount: 15.69, 
+      date: DateTime.now(), 
+      category: Category.leisure,
+    ),
+  ];
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+    body: Column(
+      children: const[
+        Text('The chart'),
+        Text('Expense list...'), 
+       ],
+      )
+   );
+ }
+}
+
